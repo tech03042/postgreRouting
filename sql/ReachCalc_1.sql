24b924d20767f514d8bf7c7c958a7752ef7fac37
Kwak MyeongSeok <tech03042@gmail.com>
2018-07-27 19:40:52 +0900

    Update ReachCalc_1.sql

--- sql/ReachCalc_1.sql
+++ sql/ReachCalc_1.sql
@@ -1,8 +1,7 @@
--- Progressing....
-
 DROP FUNCTION IF EXISTS rcalc(integer, integer);
 DROP FUNCTION IF EXISTS rcalc(integer, integer, boolean);
-CREATE FUNCTION rcalc(source integer, target integer, double_undirected boolean)
+DROP FUNCTION IF EXISTS rcalc(integer, integer, boolean, boolean);
+CREATE FUNCTION rcalc(source integer, target integer, double_undirected boolean, use_te_flag boolean)
   RETURNS BOOLEAN AS $$
 DECLARE
   affected integer;
@@ -46,12 +45,13 @@ BEGIN
 
   DROP TABLE expandTarget;
 
-  IF double_undirected
-  THEN
-    CREATE TEMP TABLE deleteNids (
-      nid int primary key
-    );
-  end if;
+  raise notice 'END';
+  CREATE TEMP TABLE deleteNids (
+    nid int primary key
+  ) ON COMMIT DROP;
+
+  ALTER TABLE te
+    ADD setNotUse boolean DEFAULT false;
 
   LOOP
     IF double_undirected
@@ -63,8 +63,9 @@ BEGIN
       FROM (SELECT visited.nid, count(*) as cnt
             FROM visited,
                  te
-            WHERE visited.nid = te.tid
-               or visited.nid = te.fid
+            WHERE (visited.nid = te.tid
+                     or visited.nid = te.fid)
+              and te.setNotUse = false
             GROUP BY visited.nid) tmp
       WHERE tmp.cnt = 2
         and tmp.nid <> source
@@ -73,22 +74,27 @@ BEGIN
       DELETE FROM visited USING deleteNids WHERE visited.nid = deleteNids.nid
                                               or visited.p2s = deleteNids.nid;
 
-      DELETE FROM te
-          USING deleteNids WHERE te.fid = deleteNids.nid
-                              or te.tid = deleteNids.nid;
+      UPDATE te
+      SET te.setNotUse = true
+      FROM deleteNids
+      WHERE te.fid = deleteNids.nid
+         or te.tid = deleteNids.nid;
+
     ELSE
       WITH a as (DELETE FROM visited
       USING (SELECT visited.nid, count(*) as cnt
              FROM visited,
                   te
-             WHERE visited.nid = te.tid
-                or visited.nid = te.fid
+             WHERE (visited.nid = te.tid
+                      or visited.nid = te.fid)
+               and te.setNoutUse = false
              GROUP BY visited.nid) tmp
       WHERE visited.nid = tmp.nid and cnt = 1
             AND visited.nid <> source and visited.nid <> target
       RETURNING visited.nid, visited.p2s)
-      DELETE
-      FROM te USING a
+      UPDATE te
+      SET te.setNotUse = true
+      FROM a
       WHERE te.tid = a.nid;
     end if;
 
@@ -100,6 +106,11 @@ BEGIN
     END IF;
   END LOOP;
 
+  IF !use_te_flag
+  THEN
+    ALTER TABLE te DROP setNotUse;
+  END IF
+  
   DROP TABLE IF EXISTS rb;
   CREATE UNLOGGED TABLE rb (
     nid INT PRIMARY KEY
@@ -110,7 +121,3 @@ BEGIN
 END;
 $$
 LANGUAGE plpgsql;
-
-
-select *
-from rcalc(13113, 435663, true);
