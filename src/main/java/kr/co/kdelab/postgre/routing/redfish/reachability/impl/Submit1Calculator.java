69a18c26c93e8032818812bf43aa33cdc12fd319
ShibaTokki <tech03042@gmail.com>
2018-07-28 01:37:10 +0900

    버그 수정
    Signed-off-by: ShibaTokki <tech03042@gmail.com>

--- src/main/java/kr/co/kdelab/postgre/routing/redfish/reachability/impl/Submit1Calculator.java
+++ src/main/java/kr/co/kdelab/postgre/routing/redfish/reachability/impl/Submit1Calculator.java
@@ -36,43 +36,6 @@ public class Submit1Calculator extends RechabliltyCalculator {
                 "    FROM findNewTarget\n" +
                 "    ON CONFLICT DO NOTHING;\n"));
         PreparedStatement deleter;
-        if (doubleUndirected) {
-            deleter = newPreparedStatement(getConnection().prepareStatement("DELETE FROM deleteNids;\n" +
-                    "INSERT INTO deleteNids (nid)\n" +
-                    "SELECT nid\n" +
-                    "FROM (SELECT visited.nid, count(*) as cnt\n" +
-                    "FROM visited,\n" +
-                    "     te\n" +
-                    "WHERE (visited.nid = te.tid\n" +
-                    "   or visited.nid = te.fid)\n" +
-                    "  and te.setNotUse = false\n" +
-                    "GROUP BY visited.nid) tmp\n" +
-                    "WHERE tmp.cnt = 2\n" +
-                    "  and tmp.nid <> " + source + "\n" +
-                    "  and tmp.nid <> " + target + ";\n" +
-                    "\n" +
-                    "DELETE FROM visited USING deleteNids WHERE visited.nid = deleteNids.nid\n" +
-                    "    or visited.p2s = deleteNids.nid;\n" +
-                    "\n" +
-                    "UPDATE te SET setNotUse = true FROM deleteNids WHERE te.fid = deleteNids.nid\n" +
-                    "  or te.tid = deleteNids.nid;\n"));
-        } else {
-            deleter = newPreparedStatement(getConnection().prepareStatement("      WITH a as (DELETE FROM visited\n" +
-                    "      USING (SELECT visited.nid, count(*) as cnt\n" +
-                    "             FROM visited,\n" +
-                    "                  te\n" +
-                    "             WHERE (visited.nid = te.tid\n" +
-                    "                      or visited.nid = te.fid)\n" +
-                    "               and te.setNotUse = false\n" +
-                    "             GROUP BY visited.nid) tmp\n" +
-                    "      WHERE visited.nid = tmp.nid and cnt = 1\n" +
-                    "            AND visited.nid <> " + source + " and visited.nid <> " + target + "\n" +
-                    "      RETURNING visited.nid, visited.p2s)\n" +
-                    "      UPDATE te\n" +
-                    "      SET setNotUse = true\n" +
-                    "      FROM a\n" +
-                    "      WHERE te.tid = a.nid;\n"));
-        }
 
         try (Statement statement = getConnection().createStatement()) {
             statement.execute("" +
@@ -100,6 +63,44 @@ public class Submit1Calculator extends RechabliltyCalculator {
             statement.execute("DROP TABLE IF EXISTS deleteNids;");
             statement.execute("CREATE UNLOGGED TABLE deleteNids (nid int primary key);");
             statement.execute("ALTER TABLE te ADD setNotUse boolean DEFAULT false;");
+
+            if (doubleUndirected) {
+                deleter = newPreparedStatement(getConnection().prepareStatement("DELETE FROM deleteNids;\n" +
+                        "INSERT INTO deleteNids (nid)\n" +
+                        "SELECT nid\n" +
+                        "FROM (SELECT visited.nid, count(*) as cnt\n" +
+                        "FROM visited,\n" +
+                        "     te\n" +
+                        "WHERE (visited.nid = te.tid\n" +
+                        "   or visited.nid = te.fid)\n" +
+                        "  and te.setNotUse = false\n" +
+                        "GROUP BY visited.nid) tmp\n" +
+                        "WHERE tmp.cnt = 2\n" +
+                        "  and tmp.nid <> " + source + "\n" +
+                        "  and tmp.nid <> " + target + ";\n" +
+                        "\n" +
+                        "DELETE FROM visited USING deleteNids WHERE visited.nid = deleteNids.nid\n" +
+                        "    or visited.p2s = deleteNids.nid;\n" +
+                        "\n" +
+                        "UPDATE te SET setNotUse = true FROM deleteNids WHERE te.fid = deleteNids.nid\n" +
+                        "  or te.tid = deleteNids.nid;\n"));
+            } else {
+                deleter = newPreparedStatement(getConnection().prepareStatement("      WITH a as (DELETE FROM visited\n" +
+                        "      USING (SELECT visited.nid, count(*) as cnt\n" +
+                        "             FROM visited,\n" +
+                        "                  te\n" +
+                        "             WHERE (visited.nid = te.tid\n" +
+                        "                      or visited.nid = te.fid)\n" +
+                        "               and te.setNotUse = false\n" +
+                        "             GROUP BY visited.nid) tmp\n" +
+                        "      WHERE visited.nid = tmp.nid and cnt = 1\n" +
+                        "            AND visited.nid <> " + source + " and visited.nid <> " + target + "\n" +
+                        "      RETURNING visited.nid, visited.p2s)\n" +
+                        "      UPDATE te\n" +
+                        "      SET setNotUse = true\n" +
+                        "      FROM a\n" +
+                        "      WHERE te.tid = a.nid;\n"));
+            }
 
             while (deleter.executeUpdate() != 0) ;
 
