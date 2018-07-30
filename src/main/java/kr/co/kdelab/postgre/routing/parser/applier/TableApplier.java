5918058a338d96f4087ca3138abfd3b75e3845d1
ShibaTokki <tech03042@gmail.com>
2018-07-30 19:09:54 +0900

    접근자 수
    Signed-off-by: ShibaTokki <tech03042@gmail.com>

--- src/main/java/kr/co/kdelab/postgre/routing/parser/applier/TableApplier.java
+++ src/main/java/kr/co/kdelab/postgre/routing/parser/applier/TableApplier.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public abstract class TableApplier {
-    TestSet testSetReader = null;
+    protected TestSet testSetReader = null;
 
     public TableApplier() {
 
