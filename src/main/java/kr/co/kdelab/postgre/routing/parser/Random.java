973a63c7cb79b27a8c55291538169741cb57c994
shibatokki <tech03042@gmail.com>
2018-08-17 10:08:07 +0900

    Weight-less 데이터를 위한 규칙 추가. 줄 당 Column 이 2개라면, WeightPolicy 변수를 확인하여 Weight 를 만듬. WeightPolicy 규칙이 DEFAULT 상태라면 기본적으로 Random 하게.
    Tab 문자로 데이터를 구분하는 규칙
    
    #으로 시작하는 줄은 Skip 하는 규칙.

--- src/main/java/kr/co/kdelab/postgre/routing/parser/Random.java
+++ src/main/java/kr/co/kdelab/postgre/routing/parser/Random.java
@@ -5,28 +5,67 @@ import kr.co.kdelab.postgre.routing.parser.internal.TestSetFormat;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 
 public class Random extends TestSet {
+
+    private WeightPolicy weightPolicy = WeightPolicy.DEFAULT;
+
+    public enum WeightPolicy {
+        DEFAULT,
+        RANDOM_WEIGHT,
+        WEIGHT_FIX_1
+    }
+
     public Random(File file) throws FileNotFoundException {
         super(file);
     }
 
-    public Random() throws FileNotFoundException {
-        super(new File("dataSet/random_5000_50000_10.txt"));
+    public void setWeightPolicy(WeightPolicy weightPolicy) {
+        this.weightPolicy = weightPolicy;
+    }
+
+    public WeightPolicy getWeightPolicy() {
+        return weightPolicy;
+    }
+
+    private double getRandomWeight() {
+        return Math.abs(Math.random() * 99) + 1;
     }
 
     @Override
     public TestSetFormat readLine() {
+        String plainData = null;
         try {
-            String plainData = readLineInternal();
-            if (plainData == null)
-                return null;
+            do {
+                plainData = readLineInternal();
+                if (plainData == null)
+                    return null;
+            } while (plainData.startsWith("#"));
+            String[] dataSplited = plainData.split("[ ,\t]");
+            int source, target;
+            double weight;
+            source = Integer.parseInt(dataSplited[0]);
+            target = Integer.parseInt(dataSplited[1]);
 
-            String[] dataSplited = plainData.split(" |,");
-            return new TestSetFormat(Integer.parseInt(dataSplited[0]), Integer.parseInt(dataSplited[1]), Math.abs(Double.parseDouble(dataSplited[2])));
-        } catch (IOException e) {
-//            Log.w(e.getMessage());
+            if (dataSplited.length < 3) {
+                switch (weightPolicy) {
+                    case WEIGHT_FIX_1:
+                        weight = 1.0f;
+                        break;
+                    case RANDOM_WEIGHT:
+                        weight = getRandomWeight();
+                        break;
+                    default:
+                        weight = getRandomWeight();
+                        break;
+                }
+            } else
+                weight = Integer.parseInt(dataSplited[2]);
+            return new TestSetFormat(source, target, weight);
+        } catch (Exception ignored) {
+            if (plainData == null)
+                plainData = "NULL";
+            System.out.printf("PARSING ERROR (%s)\n", plainData);
         }
         return null;
     }
